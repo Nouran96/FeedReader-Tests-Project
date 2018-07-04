b65cf57679c883d469b95a3c34af1ad0a1552eac
Nouran Samy <only1nour@ymail.com>
2018-07-04 23:32:01 +0200

    Modifying the RSS feeds tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -2,58 +2,63 @@
  *
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
- */
+*/
 
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
- */
+*/
 $(function() {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
     describe('RSS Feeds', function() {
+
+        // Detect whether a string is in URL format or not
+        // Reference : http://stackoverflow.com/questions/1701898/how-to-detect-whether-a-string-is-in-url-format-using-javascript
+        function isValidUrl(str) {
+            var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+            return regexp.test(str);
+        }
+
+        // For checking if a string is blank, null or undefined
+        // Reference: http://stackoverflow.com/questions/154059/how-do-you-check-for-an-empty-string-in-javascript
+        function isBlank(str) {
+            return (!str || /^\s*$/.test(str));
+        }
+
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+         * empty.
+        */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
+            expect(allFeeds instanceof Array).toBeTruthy();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        // Checks that the url of each feed is defined, valid and is not empty
         it('have their urls defined', function() {
-            for(var feed of allFeeds) {
-                // checks if the url is defined
-                expect(feed.url).toBeDefined();
 
-                // checks if the url is not empty
-                expect(feed.url).not.toBe('');
-            }
+            // Iterate over each feed
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(isBlank(feed.url)).not.toBe(true);
+                expect(isValidUrl(feed.url)).toBe(true);
+            });
+
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        // Checks that the name of each feed is defined and is not empty
         it('have their names defined', function () {
-            for (var feed of allFeeds) {
-                // checks if the name is defined
-                expect(feed.name).toBeDefined();
 
-                // checks if the name is not empty
-                expect(feed.name).not.toBe('');
-            }
+            allFeeds.forEach(function (feed) {
+                expect(feed.name).toBeDefined();
+                expect(isBlank(feed.name)).not.toBe(true);
+            });
+
         });
     });
 
